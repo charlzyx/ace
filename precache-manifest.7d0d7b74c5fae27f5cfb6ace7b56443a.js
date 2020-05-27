3436526f5c7e11f6b98e14e5bde0dec068eefcfd
xiaochao.yang <xiaochao.yang@relxtech.com>
2020-05-27 15:55:23 +0800

    Updates

---
+++ precache-manifest.7d0d7b74c5fae27f5cfb6ace7b56443a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d51e18c52ecc20fde3f35476ccef86",
+    "url": "/ace/index.html"
+  },
+  {
+    "revision": "7049e721b01995d494c1",
+    "url": "/ace/static/css/2.a2cf2a75.chunk.css"
+  },
+  {
+    "revision": "7049e721b01995d494c1",
+    "url": "/ace/static/js/2.1beb3ef7.chunk.js"
+  },
+  {
+    "revision": "f95f7ee4ee287c304acb",
+    "url": "/ace/static/js/main.a74ba0f3.chunk.js"
+  },
+  {
+    "revision": "22925f1b49af025a6fa2",
+    "url": "/ace/static/js/runtime~main.0b9693b7.js"
+  }
+]);
